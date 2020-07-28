1fa3580e7f88ab7beff9ba8ac28a5053470fa7fa
rytr823 <rytr823@gmail.com>
2020-07-28 17:24:20 +0900

    Add foreign_key at migration

--- db/migrate/20200727143555_create_contents.rb
+++ db/migrate/20200727143555_create_contents.rb
@@ -2,7 +2,7 @@ class CreateContents < ActiveRecord::Migration[6.0]
   def change
     create_table :contents do |t|
       t.string :title, null: false
-      t.references :user, null: false 
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/migrate/20200728031950_create_choices.rb
+++ db/migrate/20200728031950_create_choices.rb
@@ -2,7 +2,7 @@ class CreateChoices < ActiveRecord::Migration[6.0]
   def change
     create_table :choices do |t|
       t.string :text, null: false
-      t.references :content, null: false
+      t.references :content, null: false, foreign_key: true
       t.timestamps
     end
   end
