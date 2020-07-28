d9ab8329751b755a9b1c2ca1965409c612934d1c
rytr823 <rytr823@gmail.com>
2020-07-28 18:58:39 +0900

    Delete comment out

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,9 +1,5 @@
 class ContentsController < ApplicationController
   def index
     @contents = Content.includes(:user, :choices).order("created_at DESC")
-    # @choices = Choice.includes(:content).order("created_at DESC")
-    
-    # binding.pry
-    
   end  
 end
