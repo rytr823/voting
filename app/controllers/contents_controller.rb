83a4296db5412f3943610b3eeeacb31ac7750f4d
rytr823 <rytr823@gmail.com>
2020-07-28 17:15:32 +0900

    Add choices on post list page

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,5 +1,9 @@
 class ContentsController < ApplicationController
   def index
-    @contents = Content.includes(:user).order("created_at DESC")
+    @contents = Content.includes(:user, :choices).order("created_at DESC")
+    # @choices = Choice.includes(:content).order("created_at DESC")
+    
+    # binding.pry
+    
   end  
 end
