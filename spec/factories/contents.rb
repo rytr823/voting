3df03910e5b7f03249783ab256da568135525420
rytr823 <rytr823@gmail.com>
2020-07-31 10:26:53 +0900

    Do content model test

--- spec/factories/contents.rb
+++ spec/factories/contents.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :content do
-    
+    title { 'スマホの色は？'}
+    association :user
   end
 end
--- spec/models/choice_spec.rb
+++ spec/models/choice_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Choice, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:content) { FactoryBot.create(:content) }
+  describe '#create' do
+    before do
+      @choice = build(:choice)
+    end
+
+    # 全てのカラムが存在すれば登録できること
+    it 'is valid all column' do
+      expect(@choice).to be_valid
+    end
+    
+    # textが空であれば登録できないこと
+    it 'is invalid without a text column' do
+      @choice.text = nil
+      @choice.valid?
+      expect(@choice.errors[:text]).to include("を入力してください")
+    end
+
+    # contentが空であれば登録できないこと
+    it 'is invalid without a content' do
+      @choice.content = nil
+      @choice.invalid?
+      expect(@choice.errors[:content]).to include("を入力してください")
+    end
+  end
 end
