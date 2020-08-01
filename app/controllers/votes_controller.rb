9cd2a859a19c8e3996ccd082bb63047d1b0f0f95
rytr823 <rytr823@gmail.com>
2020-08-01 23:47:07 +0900

    Add voting function

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,25 @@
 class VotesController < ApplicationController
+  before_action :set_choice
+
+  def create
+    @vote = Vote.create(vote_params)
+    @votes = where(choice_id: @choice.id)
+    @choices = Choice.all
+  end
+
+  def destroy
+    vote = Vote.find_by(vote_params)
+    vote.destroy
+    @votes = Vote.where(choice_id: @choice.id)
+    @choices = Choice.all
+  end
+
+  private
+  def vote_params
+    params.permit(id: @choice.id).merge(user_id: current_user.id, content_id: @choice.content_id, choice_id: @choice.id)
+  end
+
+  def set_choice
+    @choice = Choice.find(params[:id])
+  end
 end
