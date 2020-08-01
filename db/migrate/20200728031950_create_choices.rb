2ad02d5117b098a0511ba9208dae0a81d5289ff0
rytr823 <rytr823@gmail.com>
2020-08-01 16:51:24 +0900

    Add votes_count column at choices table

--- db/migrate/20200728031950_create_choices.rb
+++ db/migrate/20200728031950_create_choices.rb
@@ -2,6 +2,7 @@ class CreateChoices < ActiveRecord::Migration[6.0]
   def change
     create_table :choices do |t|
       t.string :text, null: false
+      t.integer :votes_count
       t.references :content, null: false, foreign_key: true
       t.timestamps
     end
