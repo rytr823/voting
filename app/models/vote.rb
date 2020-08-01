e24101582c49ba00aacae0817eebfdf9b08ff6d8
rytr823 <rytr823@gmail.com>
2020-08-01 16:54:49 +0900

    Add user association at votes

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,5 @@
 class Vote < ApplicationRecord
+  belongs_to :user
   belongs_to :choice
 
   validates :choice, presence: true
--- db/migrate/20200728081644_create_votes.rb
+++ db/migrate/20200728081644_create_votes.rb
@@ -1,6 +1,7 @@
 class CreateVotes < ActiveRecord::Migration[6.0]
   def change
     create_table :votes do |t|
+      t.references :user, null: false, foreign_key: true
       t.references :choice, null: false, foreign_key: true
       t.timestamps
     end
