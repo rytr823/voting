9e95f9daddac4622f18319073d20ae98e3e4e0de
rytr823 <rytr823@gmail.com>
2020-08-12 16:28:44 +0900

    Add edit routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'contents#index'
-  resources :contents, only:[:index, :new, :create、:edit]
+  resources :contents, only:[:index, :new, :create, :edit]
   resources :choices do
     resources :votes, only: [:create, :destroy]
   end
