96cc6850e55436f318bde432cbe5dd9b51c95800
rytr823 <rytr823@gmail.com>
2020-08-13 21:45:52 +0900

    Delete reload.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,6 @@ require("channels")
 require('jquery')
 import "cocoon-js";
 require("../remove_btn");
-require("../reload_vote");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- app/javascript/reload_vote.js
+++
@@ -1,9 +0,0 @@
-function doReloadWithCache() {
-  window.location.reload(false);
-}
-window.addEventListener('load', function () {
-  setTimeout(doReloadWithCache, 5000);
-});
-
-
-
