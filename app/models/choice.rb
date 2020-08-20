de558c5efdf04bd5dda651d4f0e04b534fbe7012
rytr823 <rytr823@gmail.com>
2020-08-20 16:39:44 +0900

    Add validation text_or_image

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -3,7 +3,11 @@ class Choice < ApplicationRecord
   has_many :votes, dependent: :destroy
   has_one_attached :image
 
-  validates :text, presence: true
+  validates :text_or_image, presence: true
+
+  def text_or_image
+    text.presence or image.presence
+  end
 
   def vote_user(user_id)
     votes.find_by(user_id: user_id)
