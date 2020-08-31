185868124907a1043eb374524384527cbf78f03d
rytr823 <rytr823@gmail.com>
2020-08-31 15:47:49 +0900

    Refactoring votes_controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,9 +3,10 @@ class VotesController < ApplicationController
 
   def create
     @vote = Vote.new(vote_params)
-    @already_votes = @choice.content.votes
-    @already_vote = @already_votes.find_by(user_id: @vote.user_id)
-    overlap_adjustment
+    already_vote(@vote).destroy if overlap_adjustment?(@vote)
+    @vote.save
+
+    # choiceの再セット
     @choice.reload
     @choices = @choice.content.choices
   end
@@ -26,16 +27,18 @@ class VotesController < ApplicationController
     @choice = Choice.find(params[:id])
   end
 
-  def overlap_adjustment
-    if @already_votes.nil?
-      @vote.save
-    elsif @already_vote.nil?
-      @vote.save
-    elsif @already_vote.user_id == current_user.id && @vote.content_id == @already_vote.content_id
-      @already_vote.destroy
-      @vote.save
-    else
-      @vote.save
-    end
+  def overlap_adjustment?(vote)
+    is_exist_already_vote = already_vote(vote)&.user_id == current_user.id
+    is_exist_content_vote = already_vote(vote)&.content_id
+    return is_exist_already_vote && is_exist_content_vote
   end
+
+  def already_vote(vote)
+    already_votes = @choice.content.votes
+    if already_votes.nil?
+      return nil
+    end
+    already_vote = already_votes.find_by(user_id: vote.user_id)
+  end
+  
 end
