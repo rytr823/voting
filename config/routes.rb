ec5c184c9a751f1a100d690b87ea62462eaf13a5
rytr823 <rytr823@gmail.com>
2020-09-22 22:06:35 +0900

    Add search

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'contents#index'
-  resources :contents, only:[:index, :new, :create, :edit, :update, :destroy]
+  resources :contents, only:[:index, :new, :create, :edit, :update, :destroy] do
+    collection do
+      get 'search'
+    end
+  end
   resources :choices do
     resources :votes, only: [:create, :destroy]
   end
